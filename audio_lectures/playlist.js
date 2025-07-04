acdc02af7952e9a45c68e0de566e6a464cc866a7
Josh_Keck} <keckoslovakia@gmail.com>
2025-07-04 10:34:23 -0400

    try

--- audio_lectures/playlist.js
+++ audio_lectures/playlist.js
@@ -10,12 +10,12 @@
 export default [
   {
     title: "Module 1 Introduction",
-    config: "/module_1/1.0/config.js",
+    config: "./module_1/1.0/config.js",
     duration: "00:09:12.000"
   },
     {
     title: "1.1 What Counts as AI?",
-    config: "module_1/1.1/config.js",
+    config: "./module_1/1.1/config.js",
     duration: "00:15:30.000"
   }
 
