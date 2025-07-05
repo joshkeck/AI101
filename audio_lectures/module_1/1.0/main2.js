fec1b354cebf7cb682f337a363b2ffd7fd754ee6
Josh_Keck} <keckoslovakia@gmail.com>
2025-07-04 21:25:39 -0400

    TEST

--- audio_lectures/module_1/1.0/main2.js
+++ audio_lectures/module_1/1.0/main2.js
@@ -1,6 +1,6 @@
 
-import episodeUrl from './episodeurl.js';
-import configUrl  from './configurl.js';
+import episodeUrl from './episode_URL.js';
+import configUrl  from './config_URL.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   podlovePlayer('#app', configUrl, episodeUrl);
